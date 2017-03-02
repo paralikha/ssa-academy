c677a057da02ac19c36443a73005c16ed570f111
John Lioneil Dionisio <john.dionisio1@gmail.com>
2017-03-02 18:26:44 +0800

    added homepage

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+(function ($, document, window) {
+
+    // 'use strict';
+
+    $(document).ready(function (e) {
+        ///////////
+        // Inits //
+        ///////////
+
+        ///////////////
+        // Functions //
+        ///////////////
+        $(document).find('[data-img]').each(function (e) {
+            $(this).css({ 'background-img': $(this).data('img') });
+        });
+
+    });
+
+})(jQuery, document, window);
